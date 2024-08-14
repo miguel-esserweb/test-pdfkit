ad3e8a25ab327c5ff5f50036be1fe4da63fdbb74
Miguel Lopez <miguel.esserweb@gmail.com>
2024-08-14 16:54:01 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = ['pdfkit', ...(config.externals || [])];
+    }
+
+    config.resolve.alias['pdfkit'] = path.resolve(__dirname, 'node_modules/pdfkit/js/data');
+
+    config.module.rules.push({
+      test: /\.(ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'static/fonts/',
+          publicPath: '/_next/static/fonts/',
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
