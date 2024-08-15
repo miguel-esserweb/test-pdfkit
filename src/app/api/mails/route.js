8f11eeeceb1ea02d3a57e32c8218c41fcd567b55
Miguel Lopez <miguel.esserweb@gmail.com>
2024-08-15 12:13:10 -0400

    Update route.js

--- src/app/api/mails/route.js
+++ src/app/api/mails/route.js
@@ -80,7 +80,7 @@ export async function GET() {
           },
           to: [task.mail],
           subject: `Reporte para ${task.client}`,
-          text: "Reporte del mes",
+          text: "Reporte de Prueba",
           html: "**Correo enviado por Miguel**",
         });
         console.log("Correo enviado a", task.client, email.messageId);
